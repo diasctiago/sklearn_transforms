213bbd1175bf8285f067a68c313033000a4cc9dc
Tiago Dias <diasctiago@gmail.com>
2020-08-18 14:10:14 -0300

    Criação de funções

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,40 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class CreateColumnMean(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, new_column):
+        self.columns = columns
+        self.new_column = new_column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data[self.new_column] = 0
+        for column in self.columns:
+            data[self.new_column] = data[self.new_column] + data[column]
+        data[self.new_column] = data[self.new_column] / len(self.columns)
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
+    
+# All sklearn Transforms must have the `transform` and `fit` methods
+class CreateColumnSum(BaseEstimator, TransformerMixin):
+    def __init__(self, columns, new_column):
+        self.columns = columns
+        self.new_column = new_column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data[self.new_column] = 0
+        for column in self.columns:
+            data[self.new_column] = data[self.new_column] + data[column]
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/diasctiago/sklearn_transforms/',
+      author='Tiago Dias',
+      author_email='diasctiago@gmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
