0da2a009b463f74f94e75c490677b7e98f5035e5
Tiago Dias <diasctiago@gmail.com>
2020-08-18 14:32:17 -0300

    Criação de Funções

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -51,3 +51,54 @@ class CreateColumnSum(BaseEstimator, TransformerMixin):
             data[self.new_column] = data[self.new_column] + data[column]
         # Retornamos um novo dataframe com a nova coluna
         return data
+    
+# All sklearn Transforms must have the `transform` and `fit` methods
+class ModifyValuesNull(BaseEstimator, TransformerMixin):
+    def __init__(self, column, new_column):
+        self.column = column
+        self.new_column = new_column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        row_index = data.loc[data[self.column].isnull() == True].index
+        values_index = data.loc[data[self.column].isnull() == True][self.new_column].values
+        data.loc[row_index, self.column] = values_index
+        # Retornamos um novo dataframe com os dados nulos alterados
+        return data
+    
+# All sklearn Transforms must have the `transform` and `fit` methods
+class CompareDeleteValuesZero(BaseEstimator, TransformerMixin):
+    def __init__(self, columnA, columnB):
+        self.columnA = columnA
+        self.columnB = columnB
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        index_del = data.loc[data[self.columnA] == 0].loc[data[self.columnB] == 0].index
+        data.drop(index_del, inplace=True)
+        # Retornamos um novo dataframe com os dados comparados e eliminados
+        return data
+    
+    # All sklearn Transforms must have the `transform` and `fit` methods
+class CompareDeleteValuesPerfil(BaseEstimator, TransformerMixin):
+    def __init__(self, perfil):
+        self.perfil = perfil
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        index_del = data.loc[df['PERFIL'] == self.perfil].loc[data['TOTAL_NOTA']>22].loc[data['TOTAL_REPROVACOES'] == 0].index
+        data.drop(index_del, inplace=True)
+        # Retornamos um novo dataframe com os dados comparados e eliminados
+        return data
